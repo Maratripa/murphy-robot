ecd07738183f87867086f50cafdb77a127fed5d5
Martin Atria Paublo <maratripa@gmail.com>
2024-11-06 15:23:06 -0300

    fix names for 1 motor

--- src/main.cpp
+++ src/main.cpp
@@ -17,49 +17,47 @@ void parseSerialInput();
 void homeMotor();
 void homingInterrupt();
 
-AccelStepper stepper(AccelStepper::DRIVER, 52, 53);
-int smolTarget = 0;
-int prevSmolTarget = 0;
-bool smolChanged = false;
+AccelStepper stepper1(AccelStepper::DRIVER, 52, 53);
+AS5600 encoder1;
+int target1 = 0;
+int prevTarget1 = 0;
+bool changed1 = false;
+volatile bool homingComplete1 = false;
 
-PID pid(20.0, 0.002, 1.0);
+PID pid1(20.0, 0.002, 0.5);
 
-AS5600 encoder;
-double encoderOffset = 0.0;
 
 char buf[128];
 int bufPos = 0;
 
-volatile bool homingComplete = false;
-
 int32_t lastPosition;
 
 void setup() {
-  stepper.stop();
+  stepper1.stop();
   Serial.begin(9600);
 
   Wire.begin();
 
-  encoder.begin(31);
-  encoder.setDirection(AS5600_CLOCK_WISE);
+  encoder1.begin(30);
+  encoder1.setDirection(AS5600_CLOCK_WISE);
 
-  Serial.println(encoder.getAddress());
+  Serial.println(encoder1.getAddress());
 
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), homingInterrupt, RISING);
 
-  int b = encoder.isConnected();
+  int b = encoder1.isConnected();
   Serial.print("Connect: ");
   Serial.println(b);
 
   delay(1000);
-  stepper.setMaxSpeed(200 * MICROSTEPS);
+  stepper1.setMaxSpeed(200 * MICROSTEPS);
   
   delay(2000);
   homeMotor();
 
-  pid.setTolerance(1.8);
-  pid.setPoint(0);
+  pid1.setTolerance(1.8);
+  pid1.setPoint(0);
 
   //lastPosition = encoder.resetCumulativePosition(0);
 
@@ -71,74 +69,74 @@ void loop() {
 
   static uint32_t lastTime = 0; 
 
-  int32_t pos = encoder.getCumulativePosition();
+  int32_t pos = encoder1.getCumulativePosition();
   double pos_deg = pos * AS5600_RAW_TO_DEGREES;
 
-  double signal = pid.update(pos_deg);
+  double signal = pid1.update(pos_deg);
 
   if (signal != 0) {
-    stepper.setSpeed(-signal * MICROSTEPS);
-    stepper.runSpeed();
+    stepper1.setSpeed(-signal * MICROSTEPS);
+    stepper1.runSpeed();
   } else {
-    stepper.setSpeed(0);
+    stepper1.setSpeed(0);
   }
     
   if (millis() - lastTime >= 100 && signal != 0) {
     lastTime = millis();
     Serial.print(pos_deg);
     Serial.print("\t");
-    Serial.print(encoder.rawAngle() * AS5600_RAW_TO_DEGREES);
+    Serial.print(encoder1.rawAngle() * AS5600_RAW_TO_DEGREES);
     Serial.print("\n");
   }
 
 }
 
 void homingInterrupt() {
-  if (homingComplete) return;
+  if (homingComplete1) return;
   Serial.println("CLICKUP");
-  homingComplete = true;
-  stepper.stop();
+  homingComplete1 = true;
+  stepper1.stop();
 }
 
 void parseSerialInput() {
-  prevSmolTarget = smolTarget;
-  smolTarget = strtod(buf, NULL);
+  prevTarget1 = target1;
+  target1 = strtod(buf, NULL);
 
-  pid.setPoint((double)smolTarget);
+  pid1.setPoint((double)target1);
 }
 
 void homeMotor() {
   Serial.println("Starting coarse homing...");
-  homingComplete = false;
-  stepper.setSpeed(HOME_DIRECTION * HOME_SPEED * MICROSTEPS / 2);
+  homingComplete1 = false;
+  stepper1.setSpeed(HOME_DIRECTION * HOME_SPEED * MICROSTEPS / 2);
 
-  while (!homingComplete) {
-    stepper.runSpeed();
+  while (!homingComplete1) {
+    stepper1.runSpeed();
   }
 
-  stepper.stop();
+  stepper1.stop();
   Serial.println("Coarse homing complete");
   
   Serial.println("Retracting...");
-  stepper.move(-HOME_DIRECTION*30*MICROSTEPS);
-  stepper.setAcceleration(200);
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();
+  stepper1.move(-HOME_DIRECTION*30*MICROSTEPS);
+  stepper1.setAcceleration(200);
+  while (stepper1.distanceToGo() != 0) {
+    stepper1.run();
   }
 
-  homingComplete = false;
+  homingComplete1 = false;
   Serial.println("Starting fine homing...");
-  stepper.setSpeed(HOME_DIRECTION * HOME_SLOW * MICROSTEPS / 2);
+  stepper1.setSpeed(HOME_DIRECTION * HOME_SLOW * MICROSTEPS / 2);
 
-  while (!homingComplete) {
-    stepper.runSpeed();
+  while (!homingComplete1) {
+    stepper1.runSpeed();
   }
 
-  stepper.stop();
+  stepper1.stop();
   Serial.println("Fine homing complete");
 
-  stepper.setCurrentPosition(0);
-  encoder.resetCumulativePosition(0);
+  stepper1.setCurrentPosition(0);
+  encoder1.resetCumulativePosition(0);
 
   detachInterrupt(digitalPinToInterrupt(2));
 }
